34c6bd2fb69618d08d0193d52830992a7b95d73c
Vinay <VRANJAN@DMINC.COM>
2025-07-07 13:03:47 +0530

    [Inventory file changes]

--- productservices/src/main/java/com/product/microservices/productservices/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.product.microservices.productservices.repository;
-
-import com.product.microservices.productservices.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
